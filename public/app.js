a6c253d5ca2ab407c3221f88ece7a2b4ca6fcb4f
freetimezp <kraftukrnet@ukr.net>
2022-08-28 18:35:57 +0300

    09.Js-portfolio-01. Switch to another theme.

--- public/app.js
+++ public/app.js
@@ -29,6 +29,12 @@ function pageTransitions() {
             element.classList.add('active');
         }
     });
+
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode');
+    });
 }
 
 pageTransitions();
